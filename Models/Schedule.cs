0df0bc86ee2bef0fc9ba6c71df4a216b0f24ea7a
ze-fernando <zefernando.dev@gmail.com>
2024-12-04 13:23:45 -0300

    edit: models

--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -1,17 +1,10 @@
 public class Schedule
 {
+    private int Id { get; set; }
     private DateTime Date { get; set; }
     private string? Place { get; set; }
     private string? Task { get; set; }
     private Priority TaskPriority { get; set; }
     private User? TaskUser { get; set; }
 
-    public Schedule(DateTime date, string place, string task, Priority taskPriority, User taskUser)
-    {
-        Date = date;
-        Place = place;
-        Task = task;
-        TaskPriority = taskPriority;
-        TaskUser = taskUser;
-    }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,9 @@
 public class User
 {
-    private string? FirstName { get; set; }
-    private string? LastName { get; set; }
-    private string? Email { get; set; }
-    private string? Password { get; set; }
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
 
-    public User(string firstName, string lastName, string email, string password)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-        Password = password;
-    }
 }
