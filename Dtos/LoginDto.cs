5acd1992c6269da74ac1b7dc3a5a24a757cd559c
ze-fernando <zefernando.dev@gmail.com>
2024-12-04 18:20:43 -0300

    refact: dto login

--- Dtos/LoginDto.cs
+++ Dtos/LoginDto.cs
@@ -1,12 +1,13 @@
-public record LoginDto(
-    string mail,
-    string pass
-);
+using System.ComponentModel.DataAnnotations;
 
+public class LoginDto
+{
+    [Required]
+    [EmailAddress]
+    public string email { get; set; }
 
-public record SinginDto(
-    string fisrtName,
-    string lastName,
-    string mail,
-    string password
-);
+    [Required]
+    [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+    public string pass { get; set; }
+}
+
