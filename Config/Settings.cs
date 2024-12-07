49d32df94af4f9f6669e29ce91fd2efa3a951d86
ze-fernando <zefernando.dev@gmail.com>
2024-12-07 15:50:36 -0300

    create: settings

---
+++ Config/Settings.cs
@@ -0,0 +1,17 @@
+public static class Settings
+{
+    public static string Secret = "fedaf7d8863b48e197b928651854%#@%@$%@5145619287d492b708e";
+
+
+    //     public static string Secret { get; private set; }
+    //
+    //     static Settings()
+    //     {
+    //         // Carrega as variáveis de ambiente do arquivo .env
+    //         Env.Load();
+    //
+    //         // Define o valor da Secret a partir da variável de ambiente ou usa um valor padrão
+    //         Secret = Environment.GetEnvironmentVariable("MY_SECRET_KEY") 
+    //                  ?? "fedaf7d8863b48e197b928651854%#@%@$%@5145619287d492b708e";
+    //     }
+}
