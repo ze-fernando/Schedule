6bcf56285605db3ca7f7c8c329f0d924c2d6c91e
ze-fernando <zefernando.dev@gmail.com>
2024-12-08 12:57:43 -0300

    feat: migrations

---
+++ Migrations/20241208155631_InsertSchedule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace schedule.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Place = table.Column<string>(type: "TEXT", nullable: true),
+                    Task = table.Column<string>(type: "TEXT", nullable: true),
+                    TaskPriority = table.Column<int>(type: "INTEGER", nullable: false),
+                    TaskUserId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Users_TaskUserId",
+                        column: x => x.TaskUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_TaskUserId",
+                table: "Schedules",
+                column: "TaskUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
