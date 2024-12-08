7720b07944d7b191fbbf9551539688570cd866fc
ze-fernando <zefernando.dev@gmail.com>
2024-12-08 12:57:35 -0300

    feat: insert schedule ondb

--- Entites/AppDbContext.cs
+++ Entites/AppDbContext.cs
@@ -4,7 +4,14 @@ public class AppDbContext : DbContext
 {
     public DbSet<User>? Users { get; set; }
 
+    public DbSet<Schedule>? Schedules { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder options)
       => options.UseSqlite("DataSource=sqlite.db; Cache=Shared");
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(k => k.Id);
+        modelBuilder.Entity<Schedule>().HasKey(k => k.Id);
+    }
 }
