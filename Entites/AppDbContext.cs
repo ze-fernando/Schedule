a0dc091edc705514f23905b1cdcbb7a6679b9033
ze-fernando <zefernando.dev@gmail.com>
2024-12-11 14:20:40 -0300

    change dbconfig

--- Entites/AppDbContext.cs
+++ Entites/AppDbContext.cs
@@ -1,17 +1,28 @@
 using Microsoft.EntityFrameworkCore;
+using Schedule.Models;
+
+namespace Schedule.Entities;
 
 public class AppDbContext : DbContext
 {
     public DbSet<User>? Users { get; set; }
+    public DbSet<Appointment>? Schedules { get; set; }
 
-    public DbSet<Schedule>? Schedules { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-      => options.UseSqlite("DataSource=sqlite.db; Cache=Shared");
+    {
+        if (!options.IsConfigured)
+        {
+            options.UseSqlite("DataSource=sqlite.db; Cache=Shared");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>().HasKey(k => k.Id);
-        modelBuilder.Entity<Schedule>().HasKey(k => k.Id);
+        modelBuilder.Entity<Appointment>()
+            .HasOne(s => s.User)
+            .WithMany(u => u.Schedules)
+            .HasForeignKey(s => s.UserId); 
     }
 }
